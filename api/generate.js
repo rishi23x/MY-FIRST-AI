0fa3798bc69ffc0f2dc11d337882b310f4c313c3
rishi23x <subhamswn23@gmail.com>
2025-12-16 19:51:33 +0530

    api/generate.js

---
+++ api/generate.js
@@ -0,0 +1,20 @@
+export default async function handler(req, res) {
+  const userText = req.body.text;
+
+  const response = await fetch(
+    "https://api-inference.huggingface.co/models/google/flan-t5-base",
+    {
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer hf_YOUR_API_KEY",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        inputs: `Write a promotional message for this business:\n${userText}`
+      })
+    }
+  );
+
+  const data = await response.json();
+  res.status(200).json({ result: data[0].generated_text });
+}
