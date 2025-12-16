cc8355c32d09227bbb8a163fe6f35295dc7a174d
rishi23x <subhamswn23@gmail.com>
2025-12-16 20:06:01 +0530

    remove hardcoded api key

--- api/generate.js
+++ api/generate.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
     {
       method: "POST",
       headers: {
-        "Authorization": "Bearer hf_YOUR_API_KEY",
+        "Authorization": "Bearer  ${process.env.HF_API_KEY}",
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
